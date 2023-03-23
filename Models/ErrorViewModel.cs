e3b087167a6e3a246a51c66bc8d52464edd2bbbb
elisaac <ramirezisaac.1518@gmail.com>
2023-03-22 20:32:21 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace primero.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,8 @@
+namespace primero.Models;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+   
+}
